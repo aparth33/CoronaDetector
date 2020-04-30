cebd484ca8b96a70fbf259d9d0e7db88c5620129
aparth33 <aparth33@yahoo.co.in>
2020-04-30 18:12:43 +0530

    first commit

---
+++ myTraining.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+import pickle
+
+def data_split(data,ratio):
+    np.random.seed(42)
+    shuffled=np.random.permutation(len(data))
+    test_set_size=int(len(data)*ratio)
+    test_indices=shuffled[:test_set_size]
+    train_indices=shuffled[test_set_size:]
+    return data.iloc[train_indices],data.iloc[test_indices]
+
+
+if __name__=='__main__':
+
+        #Read the data
+        df=pd.read_csv("Data.csv")
+        train,test=data_split(df,0.3)
+        X_train=train[['Fever','BodyPain','Age','RunnyNose','DiffBreath']].to_numpy()
+        X_test=test[['Fever','BodyPain','RunnyNose','DiffBreath']].to_numpy()
+
+        Y_train=train[['InfectionProb']].to_numpy().reshape(1750,)
+        Y_test=test[['InfectionProb']].to_numpy().reshape(749,)
+        
+        clf=LogisticRegression()
+        clf.fit(X_train,Y_train)
+       
+       
+        # open a file, where you ant to store the data
+        file = open("model.pkl", 'wb')
+
+        # dump information to that file
+        pickle.dump(clf, file)
+        file.close()
